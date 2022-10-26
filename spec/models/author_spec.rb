c1f4116fe3f2e335c1fd217ed4bc19e7a248e14b
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-10-26 14:51:47 +0200

    Add author test I forgot to push last time

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  before :each do
+    @first_name = "Alan"
+    @last_name = "Turing"
+    @full_name = "Alan Turing"
+    @homepage = "http://wikipedia.org/Alan_Turing"
+  end
+
+  it "can be created with first name, last name and homepage" do
+    author = Author.new(first_name: @first_name, last_name: @last_name, homepage: @homepage)
+
+    expect(author.first_name).to eq(@first_name)
+    expect(author.last_name).to eq(@last_name)
+    expect(author.homepage).to eq(@homepage)
+  end
+
+  it "composes full name from first name and last name" do
+    author = Author.new(first_name: @first_name, last_name: @last_name, homepage: @homepage)
+
+    expect(author.name).to eq(@full_name)
+  end
+end
