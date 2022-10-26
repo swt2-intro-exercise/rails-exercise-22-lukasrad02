c6233ae1ea0997d01e179b682503d9a38b84aa2f
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-10-26 15:05:49 +0200

    Update authors controller to allow creation

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,20 @@
 class AuthorsController < ApplicationController
   def new
   end
+
+  def create
+    puts "HALLO WELT; DIES IST EIN TEST"
+    puts params[:author]
+
+    @author = Author.new(author_params)
+
+    @author.save
+
+    redirect_to root_path, notice: "Success!"
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
 end
