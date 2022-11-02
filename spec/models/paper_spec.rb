cfd31aa02a57f02f0a78a3d62bdb710acca4a75f
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-11-02 17:37:56 +0100

    Test paper validation

---
+++ spec/models/paper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Paper, type: :model do
+  before :each do
+    @title = "Some title"
+    @venue = "Some venue"
+    @year = 2022
+  end
+
+  it "requires last_name to be specified" do
+    paper1 = Paper.new(title: nil, venue: @venue, year: @year)
+    paper2 = Paper.new(title: "", venue: @venue, year: @year)
+
+    expect(paper1).to_not be_valid
+    expect(paper2).to_not be_valid
+  end
+end
