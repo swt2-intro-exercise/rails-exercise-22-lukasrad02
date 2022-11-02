210bbc6de23b30cf2e98dcb537f8faea4a5db6f5
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-11-02 17:43:24 +0100

    Test that paper requires venue and year

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe Paper, type: :model do
     expect(paper1).to_not be_valid
     expect(paper2).to_not be_valid
   end
+
+  it "requires venue to be specified" do
+    paper1 = Paper.new(title: @title, venue: nil, year: @year)
+    paper2 = Paper.new(title: @title, venue: "", year: @year)
+
+    expect(paper1).to_not be_valid
+    expect(paper2).to_not be_valid
+  end
+
+  it "requires year to be specified" do
+    paper1 = Paper.new(title: @title, venue: @venue, year: nil)
+    paper2 = Paper.new(title: @title, venue: @venue, year: "")
+    paper3 = Paper.new(title: @title, venue: @venue, year: "not numeric")
+
+    expect(paper1).to_not be_valid
+    expect(paper2).to_not be_valid
+    expect(paper3).to_not be_valid
+  end
 end
