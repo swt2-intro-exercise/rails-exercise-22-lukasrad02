ac158a6b50dedae71af0c427b4326f71a2753519
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-11-02 17:47:39 +0100

    Fix typo

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "New paper page", type: :feature do
-  it "should exist at 'new_paper_path' and render withour error" do
+  it "should exist at 'new_paper_path' and render without error" do
     visit new_paper_path
   end
 
