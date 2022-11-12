57063f8d1f1787d9ac54aba6d742799cdff8cfa2
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-11-12 20:32:31 +0100

    Add year filter for papers

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,9 @@
 class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
+
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, numericality: true
+
+  scope :published_in, ->(year) { where("year == ?", year) if year.present? }
 end
