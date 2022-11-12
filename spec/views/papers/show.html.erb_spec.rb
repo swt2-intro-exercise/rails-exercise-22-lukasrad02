ca4e2d622a7c3b52191e5111948743b19fc14a55
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-11-12 10:38:59 +0100

    Expect paper to show full names of authors

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "papers/show", type: :view do
   before(:each) do
+    @author = FactoryBot.create :author
+
     assign(:paper, Paper.create!(
       title: "Title",
       venue: "Venue",
-      year: 2
+      year: 2,
+      authors: [ @author ]
     ))
   end
 
@@ -14,5 +17,11 @@ RSpec.describe "papers/show", type: :view do
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Venue/)
     expect(rendered).to match(/2/)
+  end
+
+  it "shows the full names of all authors" do
+    render
+
+    expect(rendered).to have_text(@author.name)
   end
 end
